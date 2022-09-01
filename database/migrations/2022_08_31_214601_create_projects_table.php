f5f9a46ce1eda983b85853eecc478917cae764f8
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2022-09-01 10:14:58 +0200

    Update 2022_08_31_214601_create_projects_table.php

--- database/migrations/2022_08_31_214601_create_projects_table.php
+++ database/migrations/2022_08_31_214601_create_projects_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('slug', 100);
+            $table->string('slug', 100)->unique();
             $table->text('description');
             $table->timestamps();
         });
